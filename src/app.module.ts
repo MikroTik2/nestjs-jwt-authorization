aba2be47ec3a57173641fd12bc873f104c43ed03
Artur <111369356+MikroTik2@users.noreply.github.com>
2025-09-06 13:21:41 +0300

    feat: enhance app module and main bootstrap with global configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,19 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { ApiModule } from '@/api/api.module'
+import { InfraModule } from '@/infra/infra.module'
+import { LibsModule } from '@/libs/libs.module'
 
 @Module({
-	imports: []
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true
+		}),
+
+		ApiModule,
+		InfraModule,
+		LibsModule
+	]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,42 @@
-import { NestFactory } from '@nestjs/core'
+import { ClassSerializerInterceptor, Logger, ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory, Reflector } from '@nestjs/core'
+import helmet from 'helmet'
 
-import { AppModule } from './app.module'
+import { AppModule } from '@/app.module'
+import { LoggingInterceptor } from '@/common/interceptors'
+import { setupSwagger } from '@/common/utils'
+import { getCorsConfig, getHelmetConfig, getValidationPipeConfig } from '@/config'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(process.env.PORT ?? 3000)
+
+	const config = app.get(ConfigService)
+	const logger = new Logger(AppModule.name)
+
+	app.use(helmet(getHelmetConfig()))
+	app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
+	app.useGlobalInterceptors(new LoggingInterceptor())
+
+	app.setGlobalPrefix('api/v1')
+
+	app.useGlobalPipes(new ValidationPipe(getValidationPipeConfig()))
+	app.enableCors(getCorsConfig(config))
+
+	setupSwagger(app)
+
+	const port = config.getOrThrow<number>('APPLICATION_PORT')
+	const host = config.getOrThrow<string>('APPLICATION_URL')
+
+	try {
+		await app.listen(port)
+
+		logger.log(`🚀 Server is running at: ${host}`)
+		logger.log(`📄 Documentation is available at: ${host}/docs`)
+	} catch (error) {
+		logger.error(`❌ Failed to start server: ${error.message}`, error)
+		process.exit(1)
+	}
 }
 
 void bootstrap()
