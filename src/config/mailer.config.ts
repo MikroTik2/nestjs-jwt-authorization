335724d771c441d390fad4739a37a22740810472
Artur <111369356+MikroTik2@users.noreply.github.com>
2025-09-06 13:14:30 +0300

    feat: add mailer configuration for email transport and defaults

---
+++ src/config/mailer.config.ts
@@ -0,0 +1,20 @@
+import type { MailerOptions } from '@nestjs-modules/mailer'
+import { ConfigService } from '@nestjs/config'
+
+export function getMailerConfig(configService: ConfigService): MailerOptions {
+	return {
+		transport: {
+			host: configService.getOrThrow<string>('MAIL_HOST'),
+			port: configService.getOrThrow<number>('MAIL_PORT'),
+			secure: false,
+			auth: {
+				user: configService.getOrThrow<string>('MAIL_USER'),
+				pass: configService.getOrThrow<string>('MAIL_PASS')
+			}
+		},
+
+		defaults: {
+			from: `"MN Climate" <${configService.getOrThrow<string>('MAIL_USER')}>`
+		}
+	}
+}
